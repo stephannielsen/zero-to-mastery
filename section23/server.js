f0845ca474c7dc6684a43ef3b91257e2b6b30edb
Stephan Nielsen <stephannielsen@gmail.com>
2018-09-30 22:21:53 +0200

    simple express server.

--- section23/server.js
+++ section23/server.js
@@ -1,17 +1,24 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((request, response) => {
-    // console.log('headers', request.headers);
-    console.log('method', request.method);
-    console.log('url', request.url)
+const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("getting root");
+});
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+app.post('/profile', (req, res) => {
+    console.log(req.body);
     const user = {
-        name: 'John',
-        hobby: 'Skating'
+        name: 'Sally',
+        hobby: 'soccer'
     }
+    res.send(user);
+});
 
-    response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify(user));
-})
-
-server.listen(3000);
+app.listen(3000);
